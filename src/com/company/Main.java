09bdd3f2445923bff528fa97deb8ae6a865ed570
NULLWulf <89611095+NULLWulf@users.noreply.github.com>
2021-09-11 08:05:36 -0400

    removed root pw for now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         String uname = "root";
-        String password = "potter";
+        String password = ""; // enter password before runtime!
         String url = "jdbc:mysql://localhost:3306/gamedata";
         String query = "select * from weapondata";
         
